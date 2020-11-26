94c5552e5e74a1e7c797be76d90a36adac7595cd
Karol Ptak <karolptak@outlook.com>
2020-11-26 23:19:22 +0100

    Add fs4,5 simulation (aka patch up/down)

--- midi-controller/PatchChangePage.cpp
+++ midi-controller/PatchChangePage.cpp
@@ -14,8 +14,7 @@
     void PatchChangePage::button1Action() {
       button1State = !button1State;
       digitalWrite(LED_PIN, button1State); //that's only for debugging
-      MIDI.sendControlChange(midiCc1, button1State == LOW ? midiLowValue : midiHighValue, midiChannel);  
-      updateLedStrip();    
+      MIDI.sendControlChange(midiCcFs4, midiHighValue, midiChannel);  
     }
     void PatchChangePage::button2Action() {
       button2State = !button2State;
@@ -23,14 +22,11 @@
       updateLedStrip();
     }
     void PatchChangePage::button3Action() {
-      button3State = !button3State;
-      MIDI.sendControlChange(midiCc3, button3State == LOW ? midiLowValue : midiHighValue, midiChannel);
+      MIDI.sendControlChange(midiCcFs5, midiHighValue, midiChannel);
       updateLedStrip();
     }
     void PatchChangePage::button4Action() {
-      button4State = !button4State;
-      MIDI.sendControlChange(midiCc4, button4State == LOW ? midiLowValue : midiHighValue, midiChannel);
-      updateLedStrip();
+      MIDI.sendControlChange(midiCcTapTempo, midiHighValue, midiChannel);
     }
     void PatchChangePage::setup() { //setup when page is changed to this
       updateLedStrip();
@@ -39,14 +35,14 @@
     void PatchChangePage::updateLedStrip()
     {
         //led strip is temporaliry mounted upside down, so leds go in order from right to left, so need to reverse here too
-        colors[0] = hsvToRgb(patchChangePageHue, basicSat, button4State == LOW ? ledDim : ledBright);
-        colors[1] = hsvToRgb(patchChangePageHue, basicSat, button4State == LOW ? ledDim : ledBright);
-        colors[2] = hsvToRgb(patchChangePageHue, basicSat, button3State == LOW ? ledDim : ledBright);
-        colors[3] = hsvToRgb(patchChangePageHue, basicSat, button3State == LOW ? ledDim : ledBright);
-        colors[4] = hsvToRgb(patchChangePageHue, basicSat, button2State == LOW ? ledDim : ledBright);
-        colors[5] = hsvToRgb(patchChangePageHue, basicSat, button2State == LOW ? ledDim : ledBright);
-        colors[6] = hsvToRgb(patchChangePageHue, basicSat, button1State == LOW ? ledDim : ledBright);
-        colors[7] = hsvToRgb(patchChangePageHue, basicSat, button1State == LOW ? ledDim : ledBright);
+        colors[0] = hsvToRgb(0, basicSat, ledDim);
+        colors[1] = hsvToRgb(0, basicSat, ledDim);        
+        colors[2] = hsvToRgb(patchChangePageHue, basicSat, ledDim);
+        colors[3] = hsvToRgb(patchChangePageHue, basicSat, ledDim );
+        colors[4] = hsvToRgb(basicPageHue, basicSat, button2State == LOW ? ledDim : ledBright);
+        colors[5] = hsvToRgb(basicPageHue, basicSat, button2State == LOW ? ledDim : ledBright);
+        colors[6] = hsvToRgb(patchChangePageHue, basicSat, ledDim);
+        colors[7] = hsvToRgb(patchChangePageHue, basicSat, ledDim);
 
         ledStrip.write(colors, LED_COUNT);  
     }   
